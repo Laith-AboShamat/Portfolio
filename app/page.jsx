dadb765b428c23449b82cf7b6cf2e535cb267460
Laith AboShamat <aboshamatlaith@gmail.com>
2024-09-20 18:01:59 +0300

    Finshed HomePage Section

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,6 @@
+import Photo from "@/components/Photo";
+import Social from "@/components/Social";
+import Stats from "@/components/Stats";
 import { Button } from "@/components/ui/button";
 import {FiDownload} from 'react-icons/fi';
 
@@ -6,7 +9,7 @@ const Home = () => {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left ">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-center">
               Software Developer
             </span>
@@ -16,7 +19,7 @@ const Home = () => {
             <p className="max-w-[500px] mb-9 text-white/80 ">
               I excel at crafting elegant digital experiences and iam proficient in various programming languages and technologies. 
             </p>
-            <div>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button
                variant="outline"
                size="lg"
@@ -27,12 +30,20 @@ const Home = () => {
                 </span>
                   <FiDownload className="text-xl" />
               </Button>
-              <div>Socials</div>
+              <div className="mb-8 xl:mb-0">
+                <Social 
+                 containerStyles="flex gap-6"
+                 iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transtion-all duration-500"
+                />
+              </div>
             </div>
           </div>
-          <div>Photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+          <Photo />
+          </div>
         </div>
       </div>
+      <Stats/>
     </section>
   );
 }
