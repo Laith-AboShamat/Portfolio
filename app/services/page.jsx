a4f7773091b8094a146a4978e3aed7a6522ad97e
Laith AboShamat <aboshamatlaith@gmail.com>
2024-09-20 20:43:50 +0300

    adding services

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,3 +1,36 @@
+"use client";
+
+import {BsArrowDownRight} from 'react-icons/bs'
+import Link from 'next/link';
+
+const services = [
+  {
+    num: '01',
+    title: 'Web Development',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    href: ""
+  },
+  {
+    num: '02',
+    title: 'Mobile Development',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    href: ""
+  },
+  {
+    num: '03',
+    title: 'Web Development',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    href: ""
+  },
+  {
+    num: '01',
+    title: 'Web Development',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    href: ""
+  },
+]
+
+
 const Services = () => {
   return (
     <div>Services</div>
