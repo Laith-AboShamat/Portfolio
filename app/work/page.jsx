8bdec46272d8411743d36006bed122dfde74582a
Laith <aboshamatlaith@gmail.com>
2024-09-23 16:55:12 +0300

    second phase

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -16,19 +16,64 @@ const projects = [
     num: '01',
     category: 'frontend',
     title: 'project 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam cum a cumque molestias perferendis quaerat natus expedita incidunt, totam explicabo illo beatae ab! Ipsum mollitia vero corrupti eum earum sed.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
     stack: [{name: 'Html 5'},{name: 'Css 3'},{name: 'JavaScript'},],
-    image: '/',
+    image: '/assets/work/thumb1.png',
+    live: "",
+    github: "",
   },
-  
-  
-
-]
+  {
+    num: '02',
+    category: 'fullstack',
+    title: 'project 2',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    stack: [{name: 'Next.js'},{name: 'Tailwind.css'},{name: 'Node.js'},],
+    image: '/assets/work/thumb2.png',
+    live: "",
+    github: "",
+  },
+  {
+    num: '03',
+    category: 'Mobile Development',
+    title: 'project 3',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    stack: [{name: 'Flutter'},{name: 'Firebase'}],
+    image: '/assets/work/thumb3.png',
+    live: "",
+    github: "",
+  },
+];
 
 
 const Work = () => {
+  const [project, setProject] = useState(projects[0]);
   return (
-    <div>Work</div>
+    <motion.section 
+      initial={{opacity: 0}} 
+      animate={{opacity: 1}} 
+      className='min-h-[80vh] flex flex-col justify-center py-12 xl:px-0'>
+        <div className='container mx-auto'>
+          <div className='flex flex-col xl:flex-row xl:gap-[30px]'>
+            <div className='w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none'>
+                <div>
+                  {/* outline num */}
+                  <div className='text-8xl leading-none font-extrabold text-transparent text-outline'>
+                    {project.num}
+                  </div>
+                  <div>
+                    {/* project category */}
+                    <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize'>
+                      {project.category} project
+                    </h2>
+                    {/* project description */}
+                    
+                  </div>
+                </div>
+            </div>
+            <div className='w-full xl-w-[50%]'>slider</div>
+          </div>
+        </div>
+    </motion.section>
   )
 }
 
