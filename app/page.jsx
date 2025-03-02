84a6d406cac8cc9c2bde89b5f31c54bbf53b8199
Laith AboShamat <aboshamatlaith@gmail.com>
2025-03-03 01:43:35 +0200

    error fix

--- app/page.jsx
+++ app/page.jsx
@@ -15,25 +15,26 @@ const Home = () => {
               Software Developer
             </span>
             <h1 className="h1 mb-6 ">
-              Hello I'm <br/> <span className="text-accent">Laith AboShamat</span>
+              Hello I&apos;m <br/> <span className="text-accent">Laith AboShamat</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80 ">
-            I excel at crafting responsive web applications and mobile solutions, proficient in React, MERN, Next.js, AWS, and UI/UX Design.</p>
+              I excel at crafting responsive web applications and mobile solutions, proficient in React, MERN, Next.js, AWS, and UI/UX Design.
+            </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button
-               variant="outline"
-               size="lg"
-               className="uppercase flex items-center gap-2"
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
               >
                 <span>
                   Download CV
                 </span>
-                  <FiDownload className="text-xl" />
+                <FiDownload className="text-xl" />
               </Button>
               <div className="mb-8 xl:mb-0">
                 <Social 
-                 containerStyles="flex gap-6"
-                 iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transtion-all duration-500"
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transtion-all duration-500"
                 />
               </div>
             </div>
